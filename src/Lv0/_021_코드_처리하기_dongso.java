fa84a66af37b5895d3858957d832f95aa0518d69
dongso <ehdth5806@naver.com>
2023-08-24 23:57:26 +0900

    프로그래머스 코딩테스트 11번째 Commit.

--- src/Lv0/_021_코드_처리하기_dongso.java
+++ src/Lv0/_021_코드_처리하기_dongso.java
@@ -2,19 +2,48 @@ package Lv0;
 
 public class _021_코드_처리하기_dongso {
 	/*
-		두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, 
-		flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+		code를 하나씩 읽어야 한다.
+		mode 시작은 0이다.
+		code가 빈문자열이면 "EMPTY"를 리턴한다.
+		
+		mode ==> 0
+		
+		
+		mode ==> 1
+		
+		
+	
 	*/
 	public static void main(String[] args) {
-		System.out.println(solution(-4, 7, false));
+		System.out.println(solution("abc1abc1abc"));
 	}
-	public static int solution(int a, int b, boolean flag) {
-		int answer = 0;
+	public static String solution(String code) {
+		String answer = "";
+		int codeLength = code.length();
 		
-		if((a >= -1000 && a <= 1000) && (b >= -1000 && b <= 1000)) {
-			answer = flag == true ? a + b : a - b;
-		}
-        
+		if(1 <= codeLength && 100000 >= codeLength) {
+			int mode = 0;
+			char[] codeArray = code.toCharArray();
+			
+			for(int i = 0; i < codeLength; i++) {
+				if(mode == 0) {
+					if(codeArray[i] != '1' && i % 2 == 0) {
+						answer += codeArray[i];
+					} else if(codeArray[i] == '1') {
+						mode = 1;
+					}				
+				} else {
+					if(codeArray[i] != '1' && i % 2 == 1) {
+						answer += codeArray[i];
+					} else if(codeArray[i] == '1') {
+						mode = 0;
+					}
+				}
+			} // for문 끝.
+			
+			if(answer == "") answer = "EMPTY";
+			
+		} // if문 끝.
         return answer;
 	}
 }
